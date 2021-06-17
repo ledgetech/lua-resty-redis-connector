979356947b27f8408e6a9ada3184688fb2abc4a0
James Hurst <james.hurst@konghq.com>
2021-06-17 17:01:48 +0100

    fallback send_timeout in all cases
    We must do this for connector level config, as well as connection level
    config. Also moved dsn fallbacks into a utility function.

--- lib/resty/redis/connector.lua
+++ lib/resty/redis/connector.lua
@@ -186,13 +186,29 @@ end
 _M.parse_dsn = parse_dsn
 
 
-function _M.new(config)
-    -- Fill out gaps in config with any dsn params
+-- Fill out gaps in config with any dsn params
+local function apply_dsn(config)
     if config and config.url then
         local err
         config, err = parse_dsn(config)
         if err then ngx_log(ngx_ERR, err) end
     end
+end
+
+
+-- For backwards compatability; previously send_timeout was implicitly the
+-- same as read_timeout. So if only the latter is given, ensure the former
+-- matches.
+local function apply_fallback_send_timeout(config)
+    if not config.send_timeout and config.read_timeout then
+        config.send_timeout = config.read_timeout
+    end
+end
+
+
+function _M.new(config)
+    apply_dsn(config)
+    apply_fallback_send_timeout(config)
 
     local ok, config = pcall(tbl_copy_merge_defaults, config, DEFAULTS)
     if not ok then
@@ -213,20 +229,10 @@ end
 
 
 function _M.connect(self, params)
-    if params and params.url then
-        local err
-        params, err = parse_dsn(params)
-        if err then ngx_log(ngx_ERR, err) end
-    end
+    apply_dsn(params)
+    apply_fallback_send_timeout(params)
 
     params = tbl_copy_merge_defaults(params, self.config)
-
-    -- For backwards compatability; previously send_timeout was implicitly the
-    -- same as read_timeout. So if only the latter is given, ensure the former
-    -- matches.
-    if not params.send_timeout and params.read_timeout then
-        params.send_timeout = params.read_timeout
-    end
 
     if #params.sentinels > 0 then
         return self:connect_via_sentinel(params)
